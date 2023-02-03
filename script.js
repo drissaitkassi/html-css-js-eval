c4c3557a5e7a6400ac30e78937cbf77fa806d6cc
Driss <drissaitkassi@gmail.com>
2023-02-03 20:40:39 +0100

    finilize and polish ready to ship

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
+
+//------- verfier si les 3 numero de tel sont identique -------- 
+
 function handlePhoneVerification(){
+
     var teldom=document.getElementById('teldom');
     var telpor=document.getElementById('telpor');
     var telpro=document.getElementById('telpro');
-
     if(teldom.value == telpor.value && teldom.value == telpro.value){
-
         alert('les numero sont identiques ')
     }   
-
 }
 
+//------- mettre Dijon dans la valeur du champ ville  si le code postale = 21000 -------- 
+
 function handleCityValue(){
+
     var postalCode=document.getElementById('codepos');
     var ville=document.getElementById('ville');
     if(postalCode.value == 21000){
-
         ville.value = 'Dijon'
     }
-
 }
 
+//------- creation formulaire entreprise en clickant sur le button radio Cadre  -------- 
+
 function handleCardeButtons(){
-    var cadre=document.getElementById('cadre');
     var activityFieleds=document.getElementById('pro-activity');
     activityFieleds.innerHTML = " <label>Entreprise : </label><input type='text'  name='mail' id='cadre-input'/><br></br>"
 
 }
+//------- creation formulaire entite administrative  en clickant sur le button radio fonctionnaire  -------- 
 
 function handleFonctionButtons(){
-    var fonctionnaire=document.getElementById('fonctionnaire');
     var activityFieleds=document.getElementById('pro-activity');
     activityFieleds.innerHTML = " <label>Entite Administrative  : </label><input value='null' type='text' name='fonctionaire-input' id='administration'/><br></br>"
 
 }
 
+//------- creation formulaire ecole / etudiant  en clickant sur le button radio etudtiant   -------- 
+
 function handleEtudiantButtons(){
-    var etudiant=document.getElementById('etudiant');
     var activityFieleds=document.getElementById('pro-activity');
     activityFieleds.innerHTML = " <label>Ecole / Université  : </label><input type='text' value='null' name='mail' id='ecole-input'/><br></br>"
 
 }
 
+//------- creation formulaire patron en clickant sur le button radio patron  -------- 
+
 function handlePatronButtons(){
-    var patron=document.getElementById('patron');
+
     var activityFieleds=document.getElementById('pro-activity');
     activityFieleds.innerHTML = "  <label for='patron'>Les Valeur :</label>"
     +" <select id='patron-input' name='patron' value='null'>"
@@ -53,7 +59,13 @@ function handlePatronButtons(){
 }
 
 
+//------- afficher info saisie en clickant sur le button verifier   -------- 
+
+
 function afficherInfosSaisies() {
+
+    // -------recuperer les element a afficher ------
+
     var nom = document.getElementById("nom");			
     var prenom = document.getElementById("prenom");			
     var dateNaissance = document.getElementById("date-naissance");			
@@ -69,7 +81,11 @@ function afficherInfosSaisies() {
     var ecole = document.getElementById("ecole-input");			
     var patron = document.getElementById("patron-input");
 
+    //-------recuperer la div ou en va afficher les element ---------
+
     var blocDivSynthese = document.getElementById("divSynthese");
+
+    //--------- verfier le quel des champs : cadre,administration,ecole,patron n'pas null pour l'afficher dans la synthese------
 
     if(cadre !== null){
         var synthese = "<p>Synthèse de ce que vous avez saisi : \n" 
@@ -134,10 +150,13 @@ function afficherInfosSaisies() {
         + "</p>" ;
 
     }
-  
+    //---------- rendre la div synthese visible et injecter les valeur avec innerHTML-------
+
     blocDivSynthese.style.display = "block";
     blocDivSynthese.innerHTML = synthese ;
-  
+
+
+ 
 
 
                     
